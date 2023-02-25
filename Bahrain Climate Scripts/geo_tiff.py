1d9096649b7de5e72d5f24283e8a946a0ede3580
نواف الرميحي <nawafalrumaihin@gmail.com>
2023-02-26 01:39:46 +0300

    Geo_tiff of the globe
    Displays the number of bands, size (resolution), and the pixel size on the console. Opens Matplotlib to show the grayscale of the map after converted from 1d to 2d. The .geotiff is a photo of the globe extracted from NASA.

---
+++ Bahrain Climate Scripts/geo_tiff.py
@@ -0,0 +1,27 @@
+from osgeo import gdal
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+dataset = gdal.Open('land_shallow_topo_8192.tif')
+
+raster_data = dataset.ReadAsArray()
+
+num_bands = dataset.RasterCount
+print('Number of bands: {}'.format(num_bands))
+
+width = dataset.RasterXSize
+height = dataset.RasterYSize
+print('Size:', width, 'x', height)
+pixel_width, pixel_height = dataset.GetGeoTransform()[1:3]
+print('Pixel size:', pixel_width, 'x', pixel_height)
+
+# extract the red channel
+raster_data = raster_data[0]
+
+# reshape the data into a 2d array
+raster_data = raster_data.reshape((height, width))
+
+plt.imshow(raster_data, cmap='gray')
+plt.show()
+

fe9d22377d7989e5c0340daa7363d96ba0361f02
نواف الرميحي <nawafalrumaihin@gmail.com>
2023-02-26 15:25:28 +0300

    3D Converter (GIS) - Python
    Takes a .geotiff file that is 2d and converts it to 3D. The demo file is of Martin Bay in New Zealand. 
    
    Download link: https://www.linz.govt.nz/products-services/maps/new-zealand-topographic-maps/topo250-map-chooser

---
+++ elevation.py
@@ -0,0 +1,18 @@
+import rasterio
+import numpy as np
+import pyvista as pv
+
+# Load the image data from a GeoTIFF file
+with rasterio.open('newZealand.tif') as src:
+    img_data = src.read(1)
+
+# Create a mesh from the image data using PyVista
+mesh = pv.StructuredGrid()
+mesh.dimensions = np.array([img_data.shape[0], img_data.shape[1], 1])
+x, y = np.meshgrid(np.arange(mesh.dimensions[0]), np.arange(mesh.dimensions[1]))
+z = img_data.flatten()
+points = np.column_stack((x.flatten(), y.flatten(), z))
+mesh.points = points
+
+# Visualize the mesh using PyVista's `plot` function
+mesh.plot()
